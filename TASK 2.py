36a08b0ceecef369e6bbbdcd810c40ad84e41075
THANH0201 <160411131+THANH0201@users.noreply.github.com>
2024-10-31 15:51:07 +0200

    MODULE 12

---
+++ TASK 2.py
@@ -0,0 +1,34 @@
+import json
+import requests
+from datetime import datetime
+
+lat = 60
+lon = 21
+def open_weather_key():
+    return "32ffb8a7c342f1955c22fbd9c0c6d5df"
+
+def open_weather_map(city):
+    req = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_key()}"
+    #req = f"https://api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}"
+    response = requests.get(req)
+    return response.status_code, requests.get(req).json()
+
+def temp_in_celcius(kelvin):
+    return kelvin - 273.15
+
+city = input("Enter city name: ")
+
+try:
+    (result, data) = open_weather_map(city)
+    if result == 200:
+        #print(json.dumps(data, indent=2))
+        print(f"The current weather of {city}: {data['weather'][0]['description']},"
+              f"Temperature: {temp_in_celcius(data['main']['temp']):.1f} C")
+    elif result == 401:
+        print("Not valid API key")
+    elif result == 404:
+        print(f"No weather information for {city}")
+    else:
+        print(f"Error: {str(result)}")
+except requests.exceptions.RequestException as e:
+    print(f"Request could not be completed",e)
